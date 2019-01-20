1ee276f5f701f5da062a380da915efd5d1f881fb
MarcinGrzeskiewicz <grzeskiewiczmarcin@gmail.com>
2019-01-20 23:17:24 +0100

    zmienione ustawienia formatowania

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package pl.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-System.out.println("Hello, world!");
-}
+        System.out.println("Hello, world!");
+    }
 }
