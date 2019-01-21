d62cbebac34d3cc7c3ee1df6cc4aa84288d074d1
MarcinGrzeskiewicz <grzeskiewiczmarcin@gmail.com>
2019-01-21 23:15:21 +0100

    Dodane kilka nowych funkcji

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,29 @@ package pl.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Marcin");
 
-        System.out.println("Hello, world!");
-    }
+    double len = 5;
+    System.out.println("Powierzchnia kwadratu o boku " + len + "=" + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Powierzchnia prostokąta o boku " +  a + " i " + b + " = " +area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
+
 }
