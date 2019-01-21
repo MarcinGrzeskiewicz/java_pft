8ae4f458a0baa49f3930f7fc3fa2d3662681b8b3
MarcinGrzeskiewicz <grzeskiewiczmarcin@gmail.com>
2019-01-22 00:39:52 +0100

    Funkcja Distance - obliczanie odległości między punktami

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package pl.stqa.pft.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
@@ -12,12 +14,20 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Powierzchnia prostokąta o boku " + r.a + " i " + r.b + " = " + r.area());
+
+    Point P1 = new Point (2, 5);
+    Point P2 = new Point (5, 9);
+    System.out.println(Distance(P1,P2));
+
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-
+  public static double Distance(Point P1, Point P2) {
+    return Math.sqrt((P2.x-P1.x)*(P2.x-P1.x) +(P2.y-P1.y)*(P2.y-P1.y) );
+    
+  }
 
 }
