998e94f5bd7a18acb087e0e2c0d993b04c98c009
MarcinGrzeskiewicz <grzeskiewiczmarcin@gmail.com>
2019-03-07 22:05:09 +0100

    Dodany Cache

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -53,16 +53,22 @@ public class ContactHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
+  private Contacts contactCache = null;
+
    public Contacts all() {
-    Contacts contacts = new Contacts();
+     if (contactCache != null) {
+       return new Contacts (contactCache);
+     }
+
+     contactCache = new Contacts();
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for(WebElement element : elements) {
       List<WebElement> cells = element.findElements(By.tagName("td"));
       String name = cells.get(2).getText();
       String lastname = cells.get(1).getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      contacts.add(new ContactData().withId(id).withName(name).withLastname(lastname));
+      contactCache.add(new ContactData().withId(id).withName(name).withLastname(lastname));
     }
-    return contacts;
+    return new Contacts (contactCache);
   }
 }
