731015f38fb34771f95ed25f0c0ea0e2097ed9cb
MarcinGrzeskiewicz <grzeskiewiczmarcin@gmail.com>
2019-03-08 01:53:55 +0100

    TEst na porównanie adresu

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactAddressTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactAddressTest.java
@@ -1,17 +1,14 @@
 package pl.stqa.pft.addressbook.tests;
 
-
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.ContactData;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactEmailTests extends TestBase {
+public class ContactAddressTest extends TestBase {
+
   @BeforeMethod
   public void ensureContactPreconditions() {
     app.goTo().homeview();
@@ -22,21 +19,12 @@ public class ContactEmailTests extends TestBase {
   }
 
   @Test
-  public void testContactEmail() {
+  public void testContactAdress() {
     app.goTo().homeview();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
-    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
 
-  }
-
-  public static String cleaned (String email) {
-    return email.replaceAll("\\s", "").replaceAll("[-)(]", "");
-  }
-  private String mergeEmails(ContactData contact) {
-    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
-            .stream().filter((s) -> !s.equals(""))
-                    .map(ContactEmailTests::cleaned).collect(Collectors.joining("\n"));
   }
 }
